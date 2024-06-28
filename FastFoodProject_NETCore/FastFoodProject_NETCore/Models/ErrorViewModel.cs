a9f52b14f459f4c88aa7bbf8e55c77fd278ea001
Ertugrul <57365047+officialerto@users.noreply.github.com>
2024-06-28 21:31:17 +0300

    OnlineFastFoodDeliveryProject_.NETCORE
    2 video yapıldı. DEvam edilecek.

---
+++ FastFoodProject_NETCore/FastFoodProject_NETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FastFoodProject_NETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
